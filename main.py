7c853581554bf3bd9910e311c8f882e7e09218eb
Zack Dowd <zdowd2796@gmail.com>
2023-12-06 12:35:43 -0700

    added json implementation and search function

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
@@ -24,29 +27,65 @@ def generate_password():
     pyperclip.copy(text=password)
 
 
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_to_file():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }}
 
     if len(website) <= 0 or len(password) <= 0:
         messagebox.showinfo(title="Error", message="The website or password cannot be empty! Please enter a website "
                                                    "and password.")
-        return
+    else:
+        # Check for existing data.json file
+        try:
+            with open("data.json", "r") as data_file:
+                # Check for any data in data.json
+                try:
+                    # Reading old data
+                    data = json.load(data_file)
+                except json.decoder.JSONDecodeError:
+                    # Sets data to empty dictionary if no data found
+                    data = {}
+        except FileNotFoundError:
+            # Creates new data.json if not found and populates with new data
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating the old data with new data
+            data.update(new_data)
 
-    is_ok = messagebox.askokcancel(title=website,
-                                   message=f"These are the details entered: \n Email: {email} \nPassword: {password} \nIs it okay to save?")
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            website_entry.focus()
 
-    if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
-        website_entry.focus()
 
+# ----------------------------- SEARCH FUNCTION ----------------------------#
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"Information for {website} has not been stored")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -73,7 +112,7 @@ website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1)
 email_entry.insert(END, "zdowd@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
 
 # Buttons
@@ -81,5 +120,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=21, command=save_to_file)
 add_button.grid(row=4, column=1)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
